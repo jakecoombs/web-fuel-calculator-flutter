b60eb36e900c60a0bfea21a4553944e6997eb8df
Jake Coombs <jakecoombs235@gmail.com>
2021-03-27 02:56:27 +0000

    Trigger rebuild

--- webapp/src/globalStyles.ts
+++ webapp/src/globalStyles.ts
@@ -2,7 +2,7 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
 
-  body, html {
+  body {
     margin: 0;
     padding: 0;
     background: #212121;
